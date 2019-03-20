ad895ede5cb57b360146a09348bd8485c85f1396
leo10099 <hancleandro@gmail.com>
2019-03-20 13:32:00 -0300

    Edited save method on JobsController

--- controllers/JobsController.js
+++ controllers/JobsController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const logger = require('../services/logger');
 const Jobs = mongoose.model('Jobs');
 
-// * CREATE NEW JOB
-const save = async newJob => await Jobs.create(newJob).catch(e => logger.error(e));
-
-exports = { save };
+module.exports = () => ({
+  save: async newJob => await Jobs.create(newJob).catch(e => logger.error(e))
+});
