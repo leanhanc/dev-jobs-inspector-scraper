bfe876dcea7369a5c19b55e8a7077a02462f9ce9
leo10099 <hancleandro@gmail.com>
2019-03-20 13:21:35 -0300

    Create JobController and add save method

---
+++ controllers/JobsController.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const logger = require('../../services/logger');
+const Jobs = mongoose.model('Jobs');
+
+// * CREATE NEW JOB
+const save = async newJob => await Jobs.create(newJob).catch(e => logger.error(e));
+
+exports = { save };
