ce5497e6d31720e13bcac4c708d09d076d99b475
leo10099 <leandrohanc@gmail.com>
2020-12-13 12:51:01 -0300

    Add Cron Job

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 require("dotenv").config();
 require("./utils/errorHandling");
-const { SEARCH_FOR } = require("./utils/constants");
+
+const cronJobs = require("./jobs/");
 const { connectDatabase } = require("./db/connection");
-// Scrapers
-const Bumeran = require("./scraper/bumeranScraper");
-const Computrabajo = require("./scraper/computrabajoScraper");
-const Zonajobs = require("./scraper/zonajobsScraper");
 
 const bootstrap = async () => {
-	const { jobs } = await connectDatabase();
-
-	new Zonajobs(SEARCH_FOR).run(jobs);
+	const { jobs: jobsCollection } = await connectDatabase();
+	cronJobs(jobsCollection);
 };
 
 bootstrap();
--- src/jobs/index.js
+++ src/jobs/index.js
@@ -6,20 +6,20 @@ const Bumeran = require("../scraper/bumeranScraper");
 const Computrabajo = require("../scraper/computrabajoScraper");
 const Zonajobs = require("../scraper/zonajobsScraper");
 
-module.exports = async () => {
+module.exports = async jobsCollection => {
 	const jobs = cron.schedule("0 0 */3 * * *", async () => {
 		try {
 			// Computrabajo
 			const computrabajo = new Computrabajo(SEARCH_FOR);
-			await computrabajo.scraper();
+			await computrabajo.run(jobsCollection);
 
 			// Zonajobs
 			const zonajobs = new Zonajobs(SEARCH_FOR);
-			await zonajobs.scraper();
+			await zonajobs.run(jobsCollection);
 
 			// Bumeran
 			const bumeran = new Bumeran(SEARCH_FOR);
-			await bumeran.scraper();
+			await bumeran.run(jobsCollection);
 		} catch (e) {
 			logger.error(e.message);
 		}
